a8e8ca30a2267e3ce2b3f88c815d0379ae1daa5b
towergame <towergame.epic@gmail.com>
2021-06-16 01:42:46 +0300

    Improved the look and feel of the terminal

--- src/Terminal/CommandService.ts
+++ src/Terminal/CommandService.ts
@@ -1,35 +1,36 @@
+import EventEmitter from "events";
+
 export async function TerminalCommand(command: string) {
     var requestInit: RequestInit = {
-      mode: "cors",
-      credentials: "include",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({InputString: command}),
-    };  
-  
-    return fetch("https://localhost:44394/api/terminal/command", requestInit)
-      //.then(responeHandler)
-      .then((responseString) => {      
-        return responseString;
-    });
-}
+        mode: "cors",
+        credentials: "include",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ InputString: command }),
+    };
 
+    return fetch("https://localhost:44394/api/terminal/command", requestInit)
+        //.then(responeHandler)
+        .then((responseString) => {
+            return responseString;
+        });
+}
 
 function responeHandler(response: Response) {
     return response.text().then((text) => {
         const data = text && JSON.parse(text);
-  
+
         if (!response.ok) {
             if (response.status === 401) {
                 //Logout();
             }
-        
+
             const error = (data && data.message) || response.statusText;
             return Promise.reject(error);
         }
-  
+
         return data;
     });
 }
--- src/Terminal/Terminal.tsx
+++ src/Terminal/Terminal.tsx
@@ -3,62 +3,123 @@ import { Col, Row } from "reactstrap";
 import { TerminalCommand } from "./CommandService";
 import { useCookies } from "react-cookie";
 import { useHistory } from "react-router-dom";
+import "./TerminalStyle.css";
 
 export function Terminal() {
-  const [rows, setRows] = useState<string[]>(["Welcome to WOPR"]);
+    // Just some boot-up logo concepts
+    /* 
+    W     W  OOO  PPPP  RRRR
+    W     W O   O P   P R   R
+    W  W  W O   O PPPP  RRRR
+     W W W  O   O P     R R
+      W W    OOO  P     R  RR
+    */
+    /*
+    ██  ██  ██ ██████ ██████ ██████
+    ██  ██  ██ ██  ██ ██  ██ ██  ██
+     ██ ██ ██  ██  ██ █████  ████
+      ██████   ██████ ██     ██  ██
+    */
+    const [rows, setRows] = useState<string[]>([
+        "██  ██  ██ ██████ ██████ ██████",
+        "██  ██  ██ ██  ██ ██  ██ ██  ██",
+        " ██ ██ ██  ██  ██ █████  ████",
+        "  ██████   ██████ ██     ██  ██",
+        "Welcome to WOPR"
+    ]);
 
-  const [commandInput, setCommandInput] = useState<string>("");
+    const [commandInput, setCommandInput] = useState<string>("");
 
-  const history = useHistory();
-  
-  useEffect(() => {
-    setTimeout(() => {
-      setRows(rows => [...rows, "System ready"]);
-    },
-    500);
-  }, []);
+    const [processing, setProcessing] = useState<boolean>(true);
 
-  const inputCommand = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+    const history = useHistory();
 
-    setRows(rows => [...rows, commandInput]);
+    useEffect(() => {
+        setTimeout(() => {
+            document.addEventListener("click", () => {
+                let element = document.getElementById("inputter")!;
+                element.focus();
+            }); // a clinically insane thing to do, I agree
+            setRows(rows => [...rows, "System ready"]);
+            setProcessing(false); // Enable the input as everything is loaded.
+        },
+            500);
+    }, []);
 
-    TerminalCommand(commandInput)
-    .then((response) => response.text())
-    .then((response) => {
-      console.log(String(response));
-      if (String(response).startsWith("redirect-")){
-        let redirect = String(response).replace("redirect-", "");
-        history.push("/" + redirect);
-      }
-      else{
-        setRows(rows => [...rows, response]);
-      }
-    });
+    useEffect(() => {
+        let scrollingElement = document.scrollingElement!;
+        scrollingElement.scrollTop = scrollingElement.scrollHeight;
+    }, [rows, processing]);
 
-    setCommandInput("")
-  };
+    const inputCommand = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
 
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setCommandInput(e.target.value);
-  }
+        setRows(rows => [...rows, "┌─[WOPR]"]);
+        setRows(rows => [...rows, "└─▪ " + commandInput]); // Holy shit the fucking alignment
+        setProcessing(true); // We don't want the user to spam input, that'll look bad.
 
-  return (
-    <>
-      <div className="ml-5 mt-5">
-        {rows.map(function(row, i) {
-          return (
-            <Row key={i}>
-              <Col className="text-success" >{"=>"} {row}</Col>
-            </Row>
-          );
-        })}
-      </div>
-      <form className="ml-4 mt-2 d-inline-flex" onSubmit={inputCommand}>
-        <p className="bg-transparent text-success">{">"}</p>
-        <input className="bg-transparent text-success" style={{border: "none"}} value={commandInput} onChange={handleOnChange} />
-      </form>
-    </>
-  );
+        TerminalCommand(commandInput)
+            .then((response) => response.text())
+            .then((response) => {
+                console.log(String(response));
+                if (String(response).startsWith("redirect-")) {
+                    let redirect = String(response).replace("redirect-", "");
+                    history.push("/" + redirect);
+                }
+                else {
+                    setRows(rows => [...rows, response]);
+                    setProcessing(false);
+                }
+            });
+
+        setCommandInput("");
+    };
+
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        setCommandInput(e.target.value);
+    }
+
+    return (
+        <div className="terminal">
+            <div id="history" className="mt-5" style={{
+                marginLeft: "0.75rem"
+            }}>
+                {rows.map(function (row, i) {
+                    return (
+                        <Row key={i} style={{
+                            marginRight: 0,
+                            marginLeft: 0
+                        }}>
+                            <Col><pre className="terminal">{row}</pre></Col>
+                        </Row>
+                    );
+                })}
+            </div>
+            <div className="bg-transparent" style={{
+                marginLeft: "1.7rem",
+                marginBottom: "-0.5rem"
+            }} hidden={processing}>{"┌─[WOPR]"}</div>
+            <form className="mt-2 d-inline-flex" style={{
+                width: "calc(100vw - 1.75rem)",
+                overflow: "hidden",
+                height: "5%",
+                marginLeft: "1.7rem"
+            }} onSubmit={inputCommand} hidden={processing}>
+                <div className="bg-transparent" style={{
+                    width: "auto"
+                }} hidden={processing}>{"└─▪"}&nbsp;</div>
+                <input id="inputter" className="bg-transparent" style={{
+                    border: "none",
+                    outline: "none",
+                    width: "100%"
+                }} value={commandInput} onChange={handleOnChange} disabled={processing} hidden={processing} />
+            </form>
+        </div >
+        // <input className="bg-transparent text-success" style={{ border: "none" }} value={commandInput} onChange={handleOnChange} />
+        /*
+            ┌─[~][]
+            └─▪
+        */
+    );
 }
